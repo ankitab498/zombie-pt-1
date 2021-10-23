363035d8c3df2d05565f98031e644b609df41807
ankitab498 <75402577+ankitab498@users.noreply.github.com>
2021-10-23 12:17:58 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,31 @@
+var bg,bgImg;
+var player, shooterImg, shooter_shooting;
+
+
+function preload(){
+  
+  shooterImg = loadImage("assets/shooter_2.png")
+  shooter_shooting = loadImage("assets/shooter_3.png")
+
+  bgImg = loadImage("assets/bg.jpeg")
+
+}
+
+function setup() {
+
+  
+  createCanvas(windowWidth,windowHeight);
+
+  
+  
+
+}
+
+function draw() {
+  background(0); 
+
+
+
+drawSprites();
+
+}
